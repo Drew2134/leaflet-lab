1408b88551b2d9592e6fcf9913c74cff919861cf
Drew2134 <a.winchell1896@gmail.com>
2022-06-25 17:05:54 -0500

    Starting out

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const MAP = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy OpenStreetMap'
+}).addTo(MAP)
