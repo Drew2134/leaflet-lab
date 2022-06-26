6d9947740805ae44885dd7bd8d0070f86035183b
Drew2134 <a.winchell1896@gmail.com>
2022-06-26 10:55:57 -0500

    Reformat Data Style

--- js/main.js
+++ js/main.js
@@ -1,13 +1,43 @@
-const MAP = L.map('viewDiv').setView([39.8283, -98.5795], 4);
+function createMap() {
+    const MAP = L.map('viewDiv', {
+        center: [39.8283, -98.5795],
+        zoom: 4
+    });
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy OpenStreetMap'
-}).addTo(MAP)
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: "&copy OpenStreetMap"
+    }).addTo(MAP);
 
-$.ajax("data/MLBStadiumsData.geojson", {
-    dataType: "json",
-    success: function(response) {
-        L.geoJson(response).addTo(MAP);
-    }
-});
+    getData(MAP)
+};
+
+function getData(map){
+    $.ajax("data/MLBStadiumsData.geojson", {
+        dataType: "json",
+        success: function(response){
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+            
+            L.geoJson(response, {
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                },
+                style: function(feature) {
+                    switch (feature.properties.conference) {
+                        case "American League": return {color: "red"};
+                        case "National League": return {color: "blue"};
+                    }
+                } 
+            }).addTo(map);
+        }
+    });
+};
+
+$(document).ready(createMap);
