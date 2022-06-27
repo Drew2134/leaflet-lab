a9981496852b018cb04488c4ccd659f5517cd312
Drew2134 <a.winchell1896@gmail.com>
2022-06-26 21:01:43 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ function pointToLayer(feature, latlng) {
 
     var teamLogo = "img/" + feature.properties.Team.replace(/ /g, "_") + ".png";
     //build html content for popup
-    var popupContent = "<img src=" + teamLogo + ">";
+    var popupContent = "<img src=" + teamLogo + ">" + "\n<table><tr><th>Year</th><th>Value</th></tr></table>";
 
     //bind popup event to marker
     teamMarker.bindPopup(popupContent);
