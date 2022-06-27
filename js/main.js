0dc9cfe6440473af751037ce3d9e4e61fcb6c6cb
Drew2134 <a.winchell1896@gmail.com>
2022-06-26 19:07:12 -0500

    Revert to previous version

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //function to generate map and view with openstreetmap tile
 function createMap() {
     //tie map to viewDiv in HTML. Set center to center of U.S. and zoom of 4
-    var map = L.map('viewDiv', {
+    const MAP = L.map('viewDiv', {
         center: [39.8283, -98.5795],
         zoom: 4
     });
@@ -10,10 +10,10 @@ function createMap() {
     L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         maxZoom: 19,
         attribution: "&copy OpenStreetMap"
-    }).addTo(map);
+    }).addTo(MAP);
 
     //call function to get MLB data
-    getData(map)
+    getData(MAP)
 };
 
 //function to pull MLB geoJSON data and style the points
@@ -30,79 +30,29 @@ function getData(map){
                 fillOpacity: 0.8
             };
             
-            var allMLB = L.geoJson(response);
-
-            var american = L.geoJson(response, {
-                filter: function(feature, layer) {
-                    return feature.properties.conference == "American League";
-                },
-                pointToLayer: function(feature, latlng) {
+            var allMLB = L.geoJson(response, {
+                pointToLayer: function (feature, latlng){
                     //generate icon image url based on team name
-                    let intIconUrl = "img/" + feature.properties.Team.replace(/ /g, "_") + ".png";
+                    var intIconUrl = "img/" + feature.properties.Team.replace(/ /g, "_") + ".png";
                     
                     //create map icons using generated icon url
-                    let mapIcon = L.icon({
+                    var mapIcon = L.icon({
                         iconUrl: intIconUrl,
                         iconSize: [60, 60],
                         iconAnchor: [25, 25]
                     });
 
                     //generate popup content from feature properties
-                    let popupContent;
+                    var popupContent;
                     for (const property in feature.properties) {
                         popupContent += property
                     }
 
                     return L.marker(latlng, {icon: mapIcon}).bindPopup(popupContent);
                 }
-            });
-
-            var national = L.geoJson(response, {
-                filter: function(feature, layer) {
-                    return feature.properties.conference == "National League";
-                },
-                pointToLayer: function(feature, latlng) {
-                    //generate icon image url based on team name
-                    let intIconUrl = "img/" + feature.properties.Team.replace(/ /g, "_") + ".png";
-                    
-                    //create map icons using generated icon url
-                    let mapIcon = L.icon({
-                        iconUrl: intIconUrl,
-                        iconSize: [60, 60],
-                        iconAnchor: [25, 25]
-                    });
-
-                    //generate popup content from feature properties
-                    let popupContent;
-                    for (const property in feature.properties) {
-                        popupContent += property
-                    }
-
-                    return L.marker(latlng, {icon: mapIcon}).bindPopup(popupContent);
-                }
-            });
-
-            national.addTo(map);
-            american.addTo(map);
+            }).addTo(map);
         }
     });
 };
 
-//NL button bind
-$("NL").onclick(function() {
-    map.addLayer(national);
-    map.removeLayer(american);
-});
-
-//AL button bind
-$("AL").onclick(function() {
-    map.addLayer(american);
-    map.removeLayer(national);
-});
-
-//MLB button bind
-$("MLB").click(function() {
-    map.addLayer(national);
-    map.addLayer(american);
-});
 $(document).ready(createMap);
