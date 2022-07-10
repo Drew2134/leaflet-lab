50fb05b95d6169d60946a11d41444ff7e86710e9
Drew2134 <a.winchell1896@gmail.com>
2022-07-10 11:34:44 -0500

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -86,6 +86,7 @@ function pullALTeams(feature) {
 
 //function to convert default point markers to circle markers
 function pointToLayer(feature, latlng, year) {
+    console.log(year)
     var attribute = year;
 
     //generic marker options consistent to every feature
