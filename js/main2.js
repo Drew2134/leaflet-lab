f059447de33a29c994c487b315d35e3bc268971f
Drew2134 <a.winchell1896@gmail.com>
2022-07-10 09:47:50 -0500

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -28,11 +28,15 @@ function createMap() {
     //add time slider widget
     L.control.timelineSlider({
         timelineItems: ["2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"],
-        changeMap: getData
+        changeMap: changeMap
     })
     .addTo(MAP);
 };
 
+function changeMap ({value, label, map}) {
+    console.log(value, label, map);
+}
+ 
 //function to import MLB geoJSON datas
 function getData(map, layerControl) {
     let value = map.value
