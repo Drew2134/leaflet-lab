cf4322a9c4c20f0b626d7f65bf2858ffb36e7108
Drew2134 <a.winchell1896@gmail.com>
2022-07-09 23:37:50 -0500

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -25,6 +25,7 @@ function createMap() {
     layerControl.addBaseLayer(HUMAN_BASE, "Humanitarian");
     layerControl.addBaseLayer(DARK, "Dark");
 
+    //add time slider widget
     L.control.timelineSlider({
         timelineItems: ["2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"],
         changeMap: getData(MAP, layerControl)
