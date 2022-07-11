1580dc68db956b1b0b821ffda3ba98674961ec58
Drew2134 <a.winchell1896@gmail.com>
2022-07-10 20:31:41 -0500

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -132,8 +132,7 @@ function pointToLayer(feature, latlng) {
 
     //bind popup event to marker
     teamMarker.bindPopup(popupContent, {
-        offset: new L.Point(0, -geojsonMarkerOptions.radius),
-        maxWidth: "auto"
+        offset: new L.Point(0, -geojsonMarkerOptions.radius)
     });
 
     teamMarker.on({
