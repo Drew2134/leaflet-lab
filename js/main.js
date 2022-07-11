d328e451048b38864bc5a9c10650f487c94d0ea8
Drew2134 <a.winchell1896@gmail.com>
2022-07-10 22:57:35 -0500

    Update

--- js/main.js
+++ js/main.js
@@ -201,5 +201,19 @@ function updatePropSymbols() {
 }
 
 function addMapTitle(map) {
-    map.addControl(title, position = "topleft")
+    L.Control.textbox = L.Control.extend({
+		onAdd: function(map) {
+			
+		var text = L.DomUtil.create('div');
+		text.id = "info_text";
+		text.innerHTML = "<strong>text here</strong>"
+		return text;
+		},
+
+		onRemove: function(map) {
+			// Nothing to do here
+		}
+	});
+	L.control.textbox = function(opts) { return new L.Control.textbox(opts);}
+	L.control.textbox({ position: 'bottomleft' }).addTo(map);
 }
